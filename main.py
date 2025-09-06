d91deec398f8c732607dd650d0bebaa041cb471d
Aymen-Kassab <aymankassab123@gmail.com>
2025-09-06 16:51:34 +0100

    Setting up the app

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QHBoxLayout
+from PyQt5.QtGui import QIcon
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Stop Watch")
+        self.setWindowIcon(QIcon("./logo.jpg"))
+
+def main():
+    app = QApplication(sys.argv)
+    window =  MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
